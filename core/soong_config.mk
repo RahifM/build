467293d21e7c46429426706ee114c20246a6c996
Christian Oder <myself5@carbonrom.org>
2017-10-25 17:56:27 +0200

    soong: Add Qcom_bsp_legacy config
    Change-Id: Ib9284864e77a18e07f048a4afd26f2da1b6c1485

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -88,6 +88,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),';  \
 	echo ''; \
 	echo '    "ArtUseReadBarrier": $(if $(filter false,$(PRODUCT_ART_USE_READ_BARRIER)),false,true),'; \
 	echo ''; \
