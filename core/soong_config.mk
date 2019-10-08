3ec201dc089c159f6a0acabbc6be7441d2dd355f
dianlujitao <dianlujitao@lineageos.org>
2019-10-08 14:46:58 +0800

    soong_config: Reverse wait for qsee flag
     * Disabled by QCOM on P and onwards
     * https://github.com/LineageOS/android_vendor_lineage/commit/eb37bbb7cd74a2a72421e3cafad57b4afe0061d4#diff-a3506874dfcdfd07496bfeb3809d9a3c
    
    Change-Id: I3f27d399293a95ad75446482c8711adf5fed1f78
    Signed-off-by: RahifM <faizel326@gmail.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -177,6 +177,8 @@ $(call add_json_list, TargetFSConfigGen,                 $(TARGET_FS_CONFIG_GEN)
 
 $(call add_json_bool, BoardUsesQTIHardware,              $(filter true,$(BOARD_USES_QTI_HARDWARE)))
 
+$(call add_json_bool, Should_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+
 $(call add_json_map, VendorVars)
 $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call add_json_map, $(namespace))\
